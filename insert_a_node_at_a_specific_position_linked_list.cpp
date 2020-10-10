2a38d46b86dc7d24c8ab9362af2e9873f998dab4
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-10-10 10:00:34 +0200

    Insert a node at a specific position in Linked List

---
+++ insert_a_node_at_a_specific_position_linked_list.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    SinglyLinkedListNode *temp= new SinglyLinkedListNode(data);
+    temp->next=NULL;
+    SinglyLinkedListNode *traverse;
+    
+    if(head==NULL){
+        head=temp;
+    }
+    else{
+        traverse=head;
+        for(int i=0;i<position-1;i++){
+            traverse=traverse->next;
+        }
+        
+        temp->next=traverse->next;
+        traverse->next=temp;
+        
+
+    }
+
+    return head;
+
+}
+
+int main()
