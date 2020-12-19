175d23b5c50d0fa107e206fda9463843944479f2
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-12-19 09:19:13 +0100

    print function

---
+++ print_function.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    result=''
+    for i in range(1,n+1):
+        result+=str(i)
+    print(result)
+
+
